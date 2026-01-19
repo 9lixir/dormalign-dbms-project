1680019fbf6d2deda178ac83a392d5706f7c65ce
Ayushma Devkota <ayushmadevkota@Ayushmas-MacBook-Air.local>
2026-01-20 00:10:07 +0545

    added dashboard and submission pages

--- app.py
+++ app.py
@@ -70,5 +70,72 @@ def roommate():
 
     return render_template("roommate_form.html")
 
+@app.route("/submissions")
+def submissions():
+    conn = get_db_connection()
+    cur = conn.cursor()
+    
+    cur.execute("""
+        SELECT s.student_id, s.name, s.gender, s.department, s.year, h.hostel_name,
+               l.sleep_time, l.cleanliness_level, l.noise_tolerance, l.guest_preference, l.study_style,
+               r.preferred_room_type
+        FROM Student s
+        JOIN Hostel h ON s.hostel_id = h.hostel_id
+        LEFT JOIN Lifestyle_Preferences l ON s.student_id = l.student_id
+        LEFT JOIN Roommate_Request r ON s.student_id = r.student_id
+    """)
+    
+    students = cur.fetchall()
+    
+    cur.close()
+    conn.close()
+    
+    return render_template("submissions.html", students=students)
+
+@app.route("/dashboard/<int:student_id>")
+def dashboard(student_id):
+    conn = get_db_connection()
+    cur = conn.cursor()
+
+    # Fetch student info + hostel
+    cur.execute("""
+        SELECT s.name, s.department, s.year, h.hostel_name
+        FROM student s
+        JOIN hostel h ON s.hostel_id = h.hostel_id
+        WHERE s.student_id = %s
+    """, (student_id,))
+    student_data = cur.fetchone()
+
+    # Fetch lifestyle preferences
+    cur.execute("""
+        SELECT sleep_time, study_style, cleanliness_level, noise_tolerance
+        FROM lifestyle_preferences
+        WHERE student_id = %s
+    """, (student_id,))
+    preference_data = cur.fetchone()
+
+    cur.close()
+    conn.close()
+
+    student = {
+        "name": student_data[0],
+        "department": student_data[1],
+        "year": student_data[2],
+        "hostel_name": student_data[3]
+    }
+
+    preferences = {
+        "sleep_time": preference_data[0],
+        "study_style": preference_data[1],
+        "cleanliness_level": preference_data[2],
+        "noise_tolerance": preference_data[3]
+    }
+
+    return render_template(
+        "dashboard.html",
+        student=student,
+        preferences=preferences
+    )
+
 if __name__ == "__main__":
     app.run(debug=True,host="0.0.0.0",port=5001)
